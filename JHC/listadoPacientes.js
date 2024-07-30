6b0a71936c1d2667ff44f76cace96919901b877f
854_benja <854_benjamin_proyectoprueba@e2esolving.com>
2024-07-30 08:44:34 -0300

    u

--- JHC/listadoPacientes.js
+++ JHC/listadoPacientes.js
@@ -82,7 +82,7 @@ var exampleMessage = "Buen día, me comunico de la Clínica Lazarte para confirm
 let messageBox = new MessageBox(mainTable, defaultMessage)
 
 class TurnoListado extends Turno{
-    constructor(elements, paciente){
+    constructor(elements, paciente,e){
 
         let dia = elements.dia? elements.dia.innerText : null
         let fecha = elements.fecha.innerText
@@ -96,7 +96,7 @@ class TurnoListado extends Turno{
 
         this.elements.beneficio.addEventListener('mouseover', (event) => {
             let target = event.target;
-            this.showQR(target)
+            this.showQR(target,e)
         })
         this.elements.beneficio.addEventListener('mouseout', (event) => {
             this.removeQR()
@@ -107,7 +107,7 @@ class TurnoListado extends Turno{
 
         this.addCopyDataButton()
     }
-    static createFromIndex(index){
+    static createFromIndex(index,e){
         let indexString = index < 10 ? "0"+index : index
 
         let turnoElement = document.getElementById("GridContainerRow_00"+indexString)
@@ -156,14 +156,14 @@ class TurnoListado extends Turno{
 
         let paciente = new Paciente(apellido,nombre,dni,os,beneficio,cel)
 
-        return new TurnoListado(elements, paciente)
+        return new TurnoListado(elements, paciente,e)
     }
-    showQR(target){
+    showQR(target,e){
         
         // Crear el QR flotante
         let img = document.createElement('img');
         img.setAttribute("id","qr");
-        img.src = "https://image-charts.com/chart?chs=100x100&cht=qr&chl="+ this.paciente.beneficio[0] +"-"+this.paciente.beneficio[1];
+        img.src = e?"https://image-charts.com/chart?chs=100x100&cht=qr&chl="+ this.paciente.beneficio[0] +"-"+this.paciente.beneficio[1]:"https://www.i2symbol.com/images/text-symbols/square-symbol.png";
         img.style.position = 'absolute';
         img.style.zIndex = '9999';
 
@@ -211,7 +211,8 @@ function loadAgenda(){
     
     while(turno){
         i++
-        turno = TurnoListado.createFromIndex(i)
+        e = Math.random() > 0.3
+        turno = TurnoListado.createFromIndex(i,e)
     }
 
 }
